51e3e48fc8df49d13971aa0c1e57d21fb23152ea
necropolis052 <48405196+necropolis052@users.noreply.github.com>
2022-10-14 17:01:45 +0100

    Update game.vb

--- game.vb
+++ game.vb
@@ -1,16 +1,15 @@
 Imports System
 Imports System.ComponentModel.Design
-Imports System.Drawing
 Imports System.Runtime.InteropServices
 Imports System.Security.Cryptography.X509Certificates
-
-Module game
-
-    Sub Main()
+Imports System.Math
+module game
+dim bar as string = "+----------------------------------------------------------------------------------------------------------------------------------------------------------+" 
+	Sub Main()
         ''Console.WindowWidth = Console.LargestWindowWidth
         ''Console.WindowHeight = Console.LargestWindowHeight
-        Console.WindowWidth = 156
-        Console.WindowHeight = 40
+        ''Console.WindowWidth = 156
+        ''Console.WindowHeight = 40
 
         Console.ForegroundColor = ConsoleColor.Red
         Console.WriteLine("   _,-='""-.__               /\_/\")
@@ -34,29 +33,110 @@ Module game
 
     End Sub
     Sub start()
-        Dim health As Integer
-        Dim situation As Integer
-
-
-
-
-
+        Dim health As Integer = 100
+        Dim situation As Integer 
+        dim x as Integer = 0
+        dim path as string
+        
+        
+        
         situation = 1
-        Randomize()
-        ''situation = CInt(Rnd() * 3 + 1)
+        ''situation = CInt(math.ceiling(Rnd() * 3 )) + 1
         Console.WriteLine(situation)
 
         If situation = 1 Then
 
-            Console.WriteLine("+----------------------------------------------------------------------------------------------------------------------------------------------------------+")
+            Console.WriteLine(bar)
             Console.ForegroundColor = ConsoleColor.DarkMagenta
             Console.WriteLine("                                    you come across a forest. The entrance to the forest has 2 paths ")
             Console.WriteLine(" one Is thick With brambles And the other Is clear but the trees wind In a way that Is almost contorting your perspective Of how far the forest extends. ")
             Console.ForegroundColor = ConsoleColor.White
-            Console.WriteLine("+----------------------------------------------------------------------------------------------------------------------------------------------------------+")
-            Console.WriteLine()
+            console.WriteLine(bar)
+            for x = 0 To 29
+                console.WriteLine()
+            next
+            console.writeline(bar)
+            console.writeline(" Health " & health & "/100 Press 1 or 2 to select your path: ")
+            console.writeline(bar)
+            console.writeline()
+            path = console.ReadLine
+            
+            if path = "1"
+                
+            else
+            
+            end if
+        End If
+        If situation = 2 Then
+
+            Console.WriteLine(bar)
+            Console.ForegroundColor = ConsoleColor.DarkMagenta
+            Console.WriteLine("                                    you come across a forest. The entrance to the forest has 2 paths ")
+            Console.WriteLine(" one Is thick With brambles And the other Is clear but the trees wind In a way that Is almost contorting your perspective Of how far the forest extends. ")
+            Console.ForegroundColor = ConsoleColor.White
+            console.WriteLine(bar)
+            for x = 0 To 29
+                console.WriteLine()
+            next
+            console.writeline(bar)
+            console.writeline(" Health " & health & "/100 Press 1 or 2 to select your path: ")
+            console.writeline(bar)
+            console.writeline()
+            path = console.ReadLine
+            
+            if path = "1"
+                
+            else
+            
+            end if
+        End If
+        If situation = 3 Then
+
+            Console.WriteLine(bar)
+            Console.ForegroundColor = ConsoleColor.DarkMagenta
+            Console.WriteLine("                                    you come across a forest. The entrance to the forest has 2 paths ")
+            Console.WriteLine(" one Is thick With brambles And the other Is clear but the trees wind In a way that Is almost contorting your perspective Of how far the forest extends. ")
+            Console.ForegroundColor = ConsoleColor.White
+            console.WriteLine(bar)
+            for x = 0 To 29
+                console.WriteLine()
+            next
+            console.writeline(bar)
+            console.writeline(" Health " & health & "/100 Press 1 or 2 to select your path: ")
+            console.writeline(bar)
+            console.writeline()
+            path = console.ReadLine
+            
+            if path = "1"
+                
+            else
+            
+            end if
+        End If
+        If situation = 4 Then
+
+            Console.WriteLine(bar)
+            Console.ForegroundColor = ConsoleColor.DarkMagenta
+            Console.WriteLine("                                    you come across a forest. The entrance to the forest has 2 paths ")
+            Console.WriteLine(" one Is thick With brambles And the other Is clear but the trees wind In a way that Is almost contorting your perspective Of how far the forest extends. ")
+            Console.ForegroundColor = ConsoleColor.White
+            console.WriteLine(bar)
+            for x = 0 To 29
+                console.WriteLine()
+            next
+            console.writeline(bar)
+            console.writeline(" Health " & health & "/100 Press 1 or 2 to select your path: ")
+            console.writeline(bar)
+            console.writeline()
+            path = console.ReadLine
+            
+            if path = "1"
+                
+            else
+            
+            end if
         End If
 
 
     End Sub
-End Module
+end module 
