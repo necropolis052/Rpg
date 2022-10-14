2f16f2a4ac99c778c1f2fd9449fea3289d717906
sambat11 <115793534+sambat11@users.noreply.github.com>
2022-10-14 12:43:27 +0100

    Add files via upload

--- Program.vb
+++ Program.vb
@@ -1,47 +1,26 @@
 Imports System
-Imports System.ComponentModel.Design
-Imports System.Drawing
-Imports System.Runtime.InteropServices
-Imports System.Security.Cryptography.X509Certificates
 
 Module Program
-    Sub Main()
-        Console.WriteLine("Welcome to my text based rpg! ")
-        Console.WriteLine()
-        Console.WriteLine("would you like to start a game? y/n ")
-        If Console.ReadLine = "y" Then
-            Console.Clear()
-            start()
-        Else
-            End
-        End If
 
+    Dim Inventory_items() As String = {"1) Slot 1: " & "sword", "2)Slot 2: " & "sheild", "Key items:"}
+    Sub main()
+        Inventory()
     End Sub
-    Sub start()
-        Dim health As Integer
-        Dim situation As Integer
-        Console.WindowHeight = Console.LargestWindowHeight
-        Console.WindowWidth = Console.LargestWindowWidth
-        Console.SetWindowPosition(0, 0)
-        Console.windows
+    Sub Inventory()
+        Dim inv_num As Integer
+        Dim Inv_action As String
+        inv_num = 0
+        Console.WriteLine("Inventory: ")
+        For x = 0 To Inventory_items.Length - 1
 
+            Console.WriteLine(Inventory_items(inv_num))
+            inv_num = inv_num + 1
+        Next
+        Console.WriteLine("Input a number to inspect or B to close inventory: ")
+        Inv_action = Console.ReadLine
+        If Inv_action =  Then
 
-
-
-
-        situation = 1
-        Randomize()
-        ''situation = CInt(Rnd() * 3 + 1)
-        Console.WriteLine(situation)
-
-        If situation = 1 Then
-            Console.WriteLine("+----------------------------------------------------------------------------------------------------------------------------------------------------------+")
-            Console.WriteLine("                                    you come across a forest. The entrance to the forest has 2 paths ")
-            Console.WriteLine(" one Is thick With brambles And the other Is clear but the trees wind In a way that Is almost contorting your perspective Of how far the forest extends. ")
-            Console.WriteLine("+----------------------------------------------------------------------------------------------------------------------------------------------------------+")
-            Console.WriteLine()
         End If
-
 
     End Sub
 End Module
