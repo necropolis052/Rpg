09c0d93668ce94c3faf0e0d1b57d0b61f4d543e1
necropolis052 <48405196+necropolis052@users.noreply.github.com>
2022-10-14 13:47:26 +0100

    Update game.vb

--- game.vb
+++ game.vb
@@ -4,11 +4,27 @@ Imports System.Drawing
 Imports System.Runtime.InteropServices
 Imports System.Security.Cryptography.X509Certificates
 
-Module Program
+Module game
+
     Sub Main()
-        Console.WriteLine("Welcome to my text based rpg! ")
+        ''Console.WindowWidth = Console.LargestWindowWidth
+        ''Console.WindowHeight = Console.LargestWindowHeight
+        Console.WindowWidth = 156
+        Console.WindowHeight = 40
+
+        Console.ForegroundColor = ConsoleColor.Red
+        Console.WriteLine("   _,-='""-.__               /\_/\")
+        Console.Write("   `-.}       `=._,.-==-._.,  @ @._,")
+        Console.ForegroundColor = ConsoleColor.DarkRed
+        Console.WriteLine("                             Welcome to my text based rpg! ")
+        Console.ForegroundColor = ConsoleColor.White
+        Console.ForegroundColor = ConsoleColor.Red
+
+        Console.WriteLine("      `-.__   _,-.   )       _,.-'")
+        Console.WriteLine("           `""     G..m-""^m`m'")
+        Console.ForegroundColor = ConsoleColor.White
         Console.WriteLine()
-        Console.WriteLine("would you like to start a game? y/n ")
+        Console.WriteLine("                                                               would you like to start a game? y/n ")
         If Console.ReadLine = "y" Then
             Console.Clear()
             start()
@@ -20,10 +36,6 @@ Module Program
     Sub start()
         Dim health As Integer
         Dim situation As Integer
-        Console.WindowHeight = Console.LargestWindowHeight
-        Console.WindowWidth = Console.LargestWindowWidth
-        Console.SetWindowPosition(0, 0)
-        Console.windows
 
 
 
@@ -35,9 +47,12 @@ Module Program
         Console.WriteLine(situation)
 
         If situation = 1 Then
+
             Console.WriteLine("+----------------------------------------------------------------------------------------------------------------------------------------------------------+")
+            Console.ForegroundColor = ConsoleColor.DarkMagenta
             Console.WriteLine("                                    you come across a forest. The entrance to the forest has 2 paths ")
             Console.WriteLine(" one Is thick With brambles And the other Is clear but the trees wind In a way that Is almost contorting your perspective Of how far the forest extends. ")
+            Console.ForegroundColor = ConsoleColor.White
             Console.WriteLine("+----------------------------------------------------------------------------------------------------------------------------------------------------------+")
             Console.WriteLine()
         End If
