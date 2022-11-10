410126d218930345a4d078cbea412d7bdd2d8e4f
necropolis052 <48405196+necropolis052@users.noreply.github.com>
2022-11-10 14:44:37 +0000

    Update game.vb

--- game.vb
+++ game.vb
@@ -4,20 +4,28 @@ Imports System.ComponentModel.Design
 Imports System.Runtime.InteropServices
 Imports System.Security.Cryptography.X509Certificates
 Imports System.Math
-module game
-dim bar as string = "+----------------------------------------------------------------------------------------------------------------------------------------------------------+" 
-dim x as Integer
-dim name as string
-	Sub Main()
+Structure location
+    Dim Name As String
+    Dim Description As String
+    Dim North, East, South, West As Boolean
+    Dim items As String
+    Dim Npcs As String
+End Structure
+Module game
+    Dim Map(10, 10) As location
+    Dim bar As String = "+----------------------------------------------------------------------------------------------------------------------------------------------------------+"
+    Dim x As Integer
+    Dim name As String
+    Sub Main()
         ''Console.WindowWidth = Console.LargestWindowWidth
         ''Console.WindowHeight = Console.LargestWindowHeight
-        ''Console.WindowWidth = 156
-        ''Console.WindowHeight = 40
-        console.writeline()
-        console.writeline(bar)
-        for x = 0 to 12
-            console.writeline()
-        next
+        Console.WindowWidth = 156
+        Console.WindowHeight = 40
+        Console.WriteLine()
+        Console.WriteLine(bar)
+        For x = 0 To 12
+            Console.WriteLine()
+        Next
         Console.ForegroundColor = ConsoleColor.Red
         Console.WriteLine("   _,-='""-.__               /\_/\")
         Console.Write("   `-.}       `=._,.-==-._.,  @ @._,")
@@ -31,123 +39,143 @@ dim name as string
         Console.ForegroundColor = ConsoleColor.White
         Console.WriteLine()
         Console.WriteLine("                                                               Would you like to start a game? y/n ")
-        for x = 0 to 15
-            console.writeline()
-        next
-        console.writeline(bar)
-        console.writeline()
+        For x = 0 To 15
+            Console.WriteLine()
+        Next
+        Console.WriteLine(bar)
+        Console.WriteLine()
         If Console.ReadLine = "y" Then
             Console.Clear()
-            console.writeline("What is your name brave adventurer?: ")
-            name = console.ReadLine()
+            Console.WriteLine("What is your name brave adventurer?: ")
+            name = Console.ReadLine()
             start()
         Else
             End
         End If
 
+
     End Sub
     Sub start()
-        Dim health As Integer = 100 
-        dim path as string
-        dim choice as string
-        
+        Dim health As Integer = 100
+        Map(0, 0).Name = "Forest"
+        Map(0, 0).Description = ""
+        Map(0, 0).North = True
+        Map(0, 0).East = True
+        Map(0, 0).South = True
+        Map(0, 0).West = True
+        Map(0, 0).
+        Map(0, 0)
+        Map(0, 0)
+        Map(0, 0)
+        Map(0, 0)
 
-            console.writeline()
-            Console.WriteLine(bar)
-            Console.ForegroundColor = ConsoleColor.DarkMagenta
-            Console.WriteLine("                                        " & name & ", You come across a forest. The entrance to the forest has 2 paths ")
-            Console.WriteLine(" one Is thick With brambles And the other Is clear but the trees wind In a way that Is almost contorting your perspective Of how far the forest extends. ")
-            Console.ForegroundColor = ConsoleColor.White
-            console.WriteLine(bar)
-            for x = 0 To 29
-                console.WriteLine()
-            next
-            console.writeline(bar)
-            console.writeline(" Health " & health & "/100 Press 1 or 2 to select your path: ")
-            console.writeline(bar)
-            console.writeline()
-            path = console.ReadLine
-            
-            if path = "1"
-                console.clear()
-                console.writeline(bar)
-                Console.ForegroundColor = ConsoleColor.DarkMagenta
-                Console.WriteLine("  Once you've walked far enough into the brambles, a bright light appears along the path and as it dissapates a pair of wings appear floating in the air.")
-                Console.ForegroundColor = ConsoleColor.Red
-                console.WriteLine()
-                console.writeline("                                                                           The Angel")
-                console.writeline()
-                Console.ForegroundColor = ConsoleColor.DarkMagenta
-                Console.WriteLine("   ""Pleadge you alleigance to my master and I shall grant you holy guidance and the power to smite those who block your path along this grand adventure."" ")
-                Console.ForegroundColor = ConsoleColor.White
-                console.writeline(bar)
-                for x = 0 to 5
-                    console.writeline()
-                next
-                console.writeline("                                                .                                                            ,")
-                console.writeline("                                                |`.                                                        ,'|")
-                console.writeline("                                                \_`-._                                                  _,-'_/")
-                console.writeline("                                               ./ \-._`-._                                          _,-'_,-/ \,")
-                console.writeline("                                               \._/._ `._>`-.__                                __,-'<_,' _,\_,/")
-                console.writeline("                                               /_ \_.`-._\_-._ `--__                      __--' _,-_/_,-',_/ _\")
-                console.writeline("                                                /._`\_./ _`_./`.-._ `-.                ,-' _,-,'\,_'_ \,_/'_,\")
-                console.writeline("                                                 \._`/ _/ _`_./  _/`\_ `.            ,' _/'\_  \,_'_ \_ \'_,/")
-                console.writeline("                                                  /._`/ _/ _`_./` _/ `\_ `\_      _/' _/' \_ '\,_'_ \_ \'_,\")
-                console.writeline("                                                   \._`/ _/ _`_./` __/  >.  `-.,-'  ,<  \__ '\,_'_ \_ \'_,/")
-                console.writeline("                                                     /_`/._/._`_./` __.`.-\_.-`'-._/-,',__ '\,_'_,\_,\'_\")
-                console.writeline("                                                           `\._`_./`\._/_/'        `\_\_,/'\,_'_,/'")
-                for x = 0 to 9
-                    console.writeline()
-                next
-                console.writeline(bar)
-                console.writeline(" Health " & health & "/100 Press y or n to accept or decline the Angels offer: ")
-                console.writeline(bar)
-                console.writeline()
-                choice = console.ReadLine
-            else
-                console.clear()
-                console.writeline(bar)
-                Console.ForegroundColor = ConsoleColor.DarkMagenta
-                Console.WriteLine("  Once you've walked far enough into the brambles, a bright light appears along the path and as it dissapates a devil appears floating in the air.")
-                Console.ForegroundColor = ConsoleColor.Red
-                console.WriteLine()
-                console.writeline("                                                                           The Daemon")
-                console.writeline()
-                Console.ForegroundColor = ConsoleColor.DarkMagenta
-                Console.WriteLine("  ""Pleadge you alleigance to my master and I shall grant you profane guidance and the power to smite those who block your path along this grand adventure."" ")
-                Console.ForegroundColor = ConsoleColor.White
-                console.writeline(bar)
-                for x = 0 to 5
-                    console.writeline()
-                next
-                console.writeline("                                                                              ,-.")
-                console.writeline("                                                         ___,---.__          /'|`\          __,---,___")
-                console.writeline("                                                      ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-.")
-                console.writeline("                                                    ,'        |           ~'\     /`~           |        `.")
-                console.writeline("                                                   /      ___//              `. ,'          ,  , \___      \")
-                console.writeline("                                                  |    ,-'   `-.__   _         |        ,    __,-'   `-.    |")
-                console.writeline("                                                  |   /          /\_  `   .    |    ,      _/\          \   |")
-                console.writeline("                                                  \  |           \ \`-.___ \   |   / ___,-'/ /           |  /")
-                console.writeline("                                                   \  \           | `._   `\\  |  //'   _,' |           /  /")
-                console.writeline("                                                    `-.\         /'  _ `---'' , . ``---' _  `\         /,-'")
-                console.writeline("                                                       ``       /     \    ,='/ \`=.    /     \       ''")
-                console.writeline("                                                               |__   /|\_,--.,-.--,--._/|\   __|")
-                console.writeline("                                                               /  `./  \\`\ |  |  | /,//' \,'  \")
-                console.writeline("                                                              /   /     ||--+--|--+-/-|     \   \")
-                console.writeline("                                                             |   |     /'\_\_\ | /_/_/`\     |   |")
-                console.writeline("                                                              \   \__, \_     `~'     _/ .__/   /")
-                console.writeline("                                                               `-._,-'   `-._______,-'   `-._,-'")
-                for x = 0 to 5
-                    console.writeline()
-                next
-                
-                console.writeline(bar)
-                console.writeline(" Health " & health & "/100 Press y or n to accept or decline the Daemons offer: ")
-                console.writeline(bar)
-                console.writeline()
-                choice = console.ReadLine
-            end if
 
     End Sub
-    
-end module 
+    Sub boss()
+        Dim path As String
+        Dim choice As String
+
+
+        Console.WriteLine()
+        Console.WriteLine(bar)
+        Console.ForegroundColor = ConsoleColor.DarkMagenta
+        Console.WriteLine("                                        " & name & ", You come across a forest. The entrance to the forest has 2 paths ")
+        Console.WriteLine(" one Is thick With brambles And the other Is clear but the trees wind In a way that Is almost contorting your perspective Of how far the forest extends. ")
+        Console.ForegroundColor = ConsoleColor.White
+        Console.WriteLine(bar)
+        For x = 0 To 29
+            Console.WriteLine()
+        Next
+        Console.WriteLine(bar)
+        Console.WriteLine(" Health " & health & "/100 Press 1 or 2 to select your path: ")
+        Console.WriteLine(bar)
+        Console.WriteLine()
+        path = Console.ReadLine
+
+        If path = "1" Then
+            Console.Clear()
+            Console.WriteLine(bar)
+            Console.ForegroundColor = ConsoleColor.DarkMagenta
+            Console.WriteLine("  Once you've walked far enough into the brambles, a bright light appears along the path and as it dissapates a pair of wings appear floating in the air.")
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine()
+            Console.WriteLine("                                                                           The Angel")
+            Console.WriteLine()
+            Console.ForegroundColor = ConsoleColor.DarkMagenta
+            Console.WriteLine("   ""Pleadge you alleigance to my master and I shall grant you holy guidance and the power to smite those who block your path along this grand adventure."" ")
+            Console.ForegroundColor = ConsoleColor.White
+            Console.WriteLine(bar)
+            For x = 0 To 5
+                Console.WriteLine()
+            Next
+            Console.WriteLine("                                                .                                                            ,")
+            Console.WriteLine("                                                |`.                                                        ,'|")
+            Console.WriteLine("                                                \_`-._                                                  _,-'_/")
+            Console.WriteLine("                                               ./ \-._`-._                                          _,-'_,-/ \,")
+            Console.WriteLine("                                               \._/._ `._>`-.__                                __,-'<_,' _,\_,/")
+            Console.WriteLine("                                               /_ \_.`-._\_-._ `--__                      __--' _,-_/_,-',_/ _\")
+            Console.WriteLine("                                                /._`\_./ _`_./`.-._ `-.                ,-' _,-,'\,_'_ \,_/'_,\")
+            Console.WriteLine("                                                 \._`/ _/ _`_./  _/`\_ `.            ,' _/'\_  \,_'_ \_ \'_,/")
+            Console.WriteLine("                                                  /._`/ _/ _`_./` _/ `\_ `\_      _/' _/' \_ '\,_'_ \_ \'_,\")
+            Console.WriteLine("                                                   \._`/ _/ _`_./` __/  >.  `-.,-'  ,<  \__ '\,_'_ \_ \'_,/")
+            Console.WriteLine("                                                     /_`/._/._`_./` __.`.-\_.-`'-._/-,',__ '\,_'_,\_,\'_\")
+            Console.WriteLine("                                                           `\._`_./`\._/_/'        `\_\_,/'\,_'_,/'")
+            For x = 0 To 9
+                Console.WriteLine()
+            Next
+            Console.WriteLine(bar)
+            Console.WriteLine(" Health " & health & "/100 Press y or n to accept or decline the Angels offer: ")
+            Console.WriteLine(bar)
+            Console.WriteLine()
+            choice = Console.ReadLine
+        Else
+            Console.Clear()
+            Console.WriteLine(bar)
+            Console.ForegroundColor = ConsoleColor.DarkMagenta
+            Console.WriteLine("  Once you've walked far enough into the brambles, a bright light appears along the path and as it dissapates, an imp appears floating in the air.")
+            Console.ForegroundColor = ConsoleColor.Red
+            Console.WriteLine()
+            Console.WriteLine("                                                                          The Daemon")
+            Console.WriteLine()
+            Console.ForegroundColor = ConsoleColor.DarkMagenta
+            Console.WriteLine("  ""Pleadge you alleigance to my master and I shall grant you profane guidance and the power to smite those who block your path along this grand adventure."" ")
+            Console.ForegroundColor = ConsoleColor.White
+            Console.WriteLine(bar)
+            For x = 0 To 5
+                Console.WriteLine()
+            Next
+            Console.WriteLine("                                                                              ,-.")
+            Console.WriteLine("                                                         ___,---.__          /'|`\          __,---,___")
+            Console.WriteLine("                                                      ,-'    \`    `-.____,-'  |  `-.____,-'    //    `-.")
+            Console.WriteLine("                                                    ,'        |           ~'\     /`~           |        `.")
+            Console.WriteLine("                                                   /      ___//              `. ,'          ,  , \___      \")
+            Console.WriteLine("                                                  |    ,-'   `-.__   _         |        ,    __,-'   `-.    |")
+            Console.WriteLine("                                                  |   /          /\_  `   .    |    ,      _/\          \   |")
+            Console.WriteLine("                                                  \  |           \ \`-.___ \   |   / ___,-'/ /           |  /")
+            Console.Write("                                                   \  \           | `._   `\\  |  //' ")
+            Console.ForegroundColor = ConsoleColor.Blue
+            Console.Write(".")
+            Console.ForegroundColor = ConsoleColor.White
+            Console.WriteLine(" _,' |           /  /")
+            Console.WriteLine("                                                    `-.\         /'  _ `---'' , . ``---' _  `\         /,-'")
+            Console.WriteLine("                                                       ``       /     \    ,='/ \`=.    /     \       ''")
+            Console.WriteLine("                                                               |__   /|\_,--.,-.--,--._/|\   __|")
+            Console.WriteLine("                                                               /  `./  \\`\ |  |  | /,//' \,'  \")
+            Console.WriteLine("                                                              /   /     ||--+--|--+-/-|     \   \")
+            Console.WriteLine("                                                             |   |     /'\_\_\ | /_/_/`\     |   |")
+            Console.WriteLine("                                                              \   \__, \_     `~'     _/ .__/   /")
+            Console.WriteLine("                                                               `-._,-'   `-._______,-'   `-._,-'")
+            For x = 0 To 5
+                Console.WriteLine()
+            Next
+
+            Console.WriteLine(bar)
+            Console.WriteLine(" Health " & health & "/100 Press y or n to accept or decline the Daemons offer: ")
+            Console.WriteLine(bar)
+            Console.WriteLine()
+            choice = Console.ReadLine
+        End If
+
+    End Sub
+
+End Module
