b9ae0330b2f3e26e4c35f81be5a7ec21b41fdb99
sambat11 <115793534+sambat11@users.noreply.github.com>
2022-11-10 13:26:43 +0000

    Update inventory.vb

--- inventory.vb
+++ inventory.vb
@@ -21,7 +21,7 @@ Module Program
                 Console.WriteLine(Inventory_items(inv_num))
                 inv_num = inv_num + 1
             Next
-            Console.WriteLine("Input a number to inspect or B to close inventory: ")
+            Console.WriteLine("Input a number to inspect or B to close inventory: ") ' gives the player choice to either close inventory or access item options
             Inv_action = Console.ReadLine()
             Inv_action = CStr(Inv_action)
             If Inv_action = "1" Or Inv_action = "2" Or Inv_action = "3" Or Inv_action = "4" Or Inv_action = "5" Or Inv_action = "6" Then
@@ -30,10 +30,9 @@ Module Program
 
             If Inv_action = "b" Or Inv_action = "B" Then
                 inv_end = True
-            End If
+            End If ' if the player chooses to close inventory it closes inv
 
         Loop Until inv_end = True
         Console.WriteLine("cheese")
     End Sub
 End Module
-
